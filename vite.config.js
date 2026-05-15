02626a7e05290c88f9b89e013f9dfd47df66a78a
benniejung <ybe1027@hansung.ac.kr>
2026-05-15 21:42:22 +0900

    refactor: imagemin 패키지 설치

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import ViteWebp from 'vite-plugin-webp';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin'
+
+// The minifiers you want to use:
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminWebp from 'imagemin-webp'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteWebp({
-      extensions: ['png', 'jpg'],
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+        },
+      },
     }),
   ],
 });
