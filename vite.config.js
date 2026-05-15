806057aa3dcb2ae8c8c69ab7c902e087e5b84c23
benniejung <ybe1027@hansung.ac.kr>
2026-05-15 21:48:36 +0900

    refactor: vite 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import viteImagemin from '@vheemstra/vite-plugin-imagemin'
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
 
 // The minifiers you want to use:
-import imageminMozjpeg from 'imagemin-mozjpeg'
-import imageminWebp from 'imagemin-webp'
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminWebp from 'imagemin-webp';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,10 +15,14 @@ export default defineConfig({
     viteImagemin({
       plugins: {
         jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        svg: imageminSvgo(),
       },
       makeWebp: {
         plugins: {
           jpg: imageminWebp(),
+          png: imageminWebp(),
+          svg: imageminWebp(),
         },
       },
     }),
